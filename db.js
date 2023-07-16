26af2d1b53f6fcc9207fc34ab7dd2e6b5247327d
Divya Rani <101196300+divy08r@users.noreply.github.com>
2023-07-16 13:21:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ async function main() {
   await mongoose.connect(database);
   console.log("Connected to mongoose");
   const fetched_data = await mongoose.connection.db.collection("fooditems");
-
+  console.log(fetched_data);
   fetched_data.find({}).toArray(async function (err, data) {
     const foodcategory = await mongoose.connection.db.collection("foodcategories");
 
